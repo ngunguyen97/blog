93a3fc361eba71372636c729291957cb2491f32f
lucynguyen <ngu.nguyen@kyanon.digital>
2020-10-24 17:52:10 +0700

    adds author resource

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,7 @@ Route::get('/', function () {
 
 Route::post('/books', 'BookController@store');
 Route::patch('/books/{book}', 'BookController@update');
+Route::delete('/books/{book}', 'BookController@destroy');
+
+
+Route::post('/author', 'AuthorController@store');
---
+++ tests/Feature/AuthorManagementTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Author;
+use Carbon\Carbon;
+
+class AuthorManagementTest extends TestCase
+{
+    use RefreshDatabase;
+    /** @test */
+    public function an_author_can_be_created() {
+        $this->withoutExceptionHandling();
+        $this->post('/author',[
+            'name' => 'Author Name',
+            'dob'=> '2019/05/03'
+        ]);
+
+        $author = Author::all();
+
+        $this->assertCount(1, $author);
+        $this->assertInstanceOf(Carbon::class, $author->first()->dob);
+        $this->assertEquals('03/05/2019', $author->first()->dob->format('d/m/Y'));
+    }
+}
